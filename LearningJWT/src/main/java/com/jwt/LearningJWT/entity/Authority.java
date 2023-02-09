27fb3a36e5fdf5dd0f69aaf43e38aea3c6980fa8
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-02-10 02:53:32 +0900

    로그인 API까지 JWT활용하여 구현

---
+++ LearningJWT/src/main/java/com/jwt/LearningJWT/entity/Authority.java
@@ -0,0 +1,19 @@
+package com.jwt.LearningJWT.entity;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authority")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Authority {
+
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
